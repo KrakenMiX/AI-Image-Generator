2d54739f21ce6b5053583941d209cd1797ea2f7d
KrakenMiX <dghaz.xrpl5@gmail.com>
2024-02-06 06:52:38 +0700

    Buat register dan login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AipromptController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,18 @@ use App\Http\Controllers\AipromptController;
 |
 */
 
-Route::get('/', [AipromptController::class, 'index'])->name('aiprompt');
+//REGISTER
+Route::get('register', [RegisterController::class, 'register'])->name('register');
+Route::post('register/action', [RegisterController::class, 'actionregister'])->name('actionregister');
+
+//LOGIN
+Route::get('/', [LoginController::class, 'login'])->name('login');
+Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');
+
+Route::get('aiprompt', [AipromptController::class, 'index'])->name('aiprompt')->middleware('auth');
+Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
+
 Route::post('generate', [AipromptController::class, 'generateImage'])->name('generate');
 Route::get('result', [AipromptController::class, 'image'])->name('airesult');
 Route::get('download', [AipromptController::class, 'download'])->name('download');
+
