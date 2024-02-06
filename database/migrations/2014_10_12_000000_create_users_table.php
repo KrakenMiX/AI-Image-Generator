57fa763a125ad42c057849b7487be2a5fcbfc9e0
MRHRTZ <hanifsyauqi61@gmail.com>
2024-02-06 11:30:03 +0700

    migrate users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->integerIncrements('id_user')->unsigned();
+            $table->string('username');
             $table->string('password');
+            $table->string('email')->unique();
+            $table->string('verify_key', 100)->nullable();
+            $table->integer('active')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
