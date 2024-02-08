ac20c687697e2da9be22ffb9292659f46684cd89
badharalfath <outlockalfath@gmail.com>
2024-02-08 13:43:04 +0700

    tambah galeri

--- resources/views/galeri.blade.php
+++ resources/views/galeri.blade.php
@@ -2,16 +2,39 @@
 
 @section('content')
 <div class="wrapper-galeri">
-    <div class="title-area loading">
-        <span class="title">My Galery</span>
+    <div class="title-area-galeri ">
+        <span class="title-galeri">My Galery</span>
     </div>
+    <hr>
     <div class="text-white px-3 py-3" style="">
         <div class="image-grid">
-            <img class="image-grid-col-2 image-grid-row-2" src="https://source.unsplash.com/random/300x300?v=1" alt="architecture">
-            <img src="https://source.unsplash.com/random/300x300?v=2" alt="architecture">
-            <img src="https://source.unsplash.com/random/300x300?v=3" alt="architecture">
-            <img src="https://source.unsplash.com/random/300x300?v=4" alt="architecture">
-            <img src="https://source.unsplash.com/random/300x300?v=5" alt="architecture">
+            <a href="https://source.unsplash.com/random/300x300?v=1" target="_blank" rel="noopener noreferrer" class="image-link">
+                <img src="https://source.unsplash.com/random/300x300?v=1" alt="architecture" class="hover-zoom">
+            </a>
+            <a href="https://source.unsplash.com/random/300x300?v=2" target="_blank" rel="noopener noreferrer" class="image-link">
+                <img src="https://source.unsplash.com/random/300x300?v=2" alt="architecture" class="hover-zoom">
+            </a>
+            <a href="https://source.unsplash.com/random/300x300?v=3" target="_blank" rel="noopener noreferrer" class="image-link">
+                <img src="https://source.unsplash.com/random/300x300?v=3" alt="architecture" class="hover-zoom">
+            </a>
+            <a href="https://source.unsplash.com/random/300x300?v=4" target="_blank" rel="noopener noreferrer" class="image-link">
+                <img src="https://source.unsplash.com/random/300x300?v=4" alt="architecture" class="hover-zoom">
+            </a>
+            <a href="https://source.unsplash.com/random/300x300?v=5" target="_blank" rel="noopener noreferrer" class="image-link">
+                <img src="https://source.unsplash.com/random/300x300?v=5" alt="architecture" class="hover-zoom">
+            </a>
+            <a href="https://source.unsplash.com/random/300x300?v=2" target="_blank" rel="noopener noreferrer" class="image-link">
+                <img src="https://source.unsplash.com/random/300x300?v=2" alt="architecture" class="hover-zoom">
+            </a>
+            <a href="https://source.unsplash.com/random/300x300?v=2" target="_blank" rel="noopener noreferrer" class="image-link">
+                <img src="https://source.unsplash.com/random/300x300?v=2" alt="architecture" class="hover-zoom">
+            </a>
+            <a href="https://source.unsplash.com/random/300x300?v=2" target="_blank" rel="noopener noreferrer" class="image-link">
+                <img src="https://source.unsplash.com/random/300x300?v=2" alt="architecture" class="hover-zoom">
+            </a>
+            <a href="https://source.unsplash.com/random/300x300?v=2" target="_blank" rel="noopener noreferrer" class="image-link">
+                <img src="https://source.unsplash.com/random/300x300?v=2" alt="architecture" class="hover-zoom">
+            </a>
         </div>
 
             <div class="loading-text">
