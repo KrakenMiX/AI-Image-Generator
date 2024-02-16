19ad2ed16fe8319371463eb1ab8b7345bc43b2b6
MRHRTZ <hanifsyauqi61@gmail.com>
2024-02-16 15:36:12 +0700

    add informasi proyek

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>IGenAI Art</title>
-
+    <link rel="shortcut icon" href="img/icon.png" type="image/png">
     <!-- Custom styles for this template -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
