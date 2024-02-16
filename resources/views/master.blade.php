9cd1dc915ca6170ad7cf87a330ca689f50c71ffc
MRHRTZ <hanifsyauqi61@gmail.com>
2024-02-16 15:45:00 +0700

    fix one word

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>IGenAI Art</title>
+    <title>GenAI Art</title>
     <link rel="shortcut icon" href="img/icon.png" type="image/png">
     <!-- Custom styles for this template -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
